29c068f67c8e38f494dbe81983fe5a4be2508e61
Pedro Sánchez Nery <pedro.sanchez.nery@gmail.com>
2026-04-04 12:18:41 -0500

    Update create-checkout-session.ts

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2022-11-15' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2022-11-15' });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") return res.status(405).end();
